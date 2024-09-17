a2a15ae2f26ae026d1727aa3f499f38673f61e2c
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-09-17 23:44:17 +0300

    header

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import styled from "styled-components";
 import {Logo} from "../../components/Logo/Logo";
-import {Menu} from "../../components/Menu/Menu";
+
+import {Container} from "../../components/Container";
+import {FlexWrapper} from "../../components/FlexWrapper";
+import {HeaderMenu} from "./HeaderMenu/HeaderMenu";
+
 
 
 const items = ['Home', 'Skills', 'Works','Testimony','Contact']
@@ -9,15 +13,27 @@ const items = ['Home', 'Skills', 'Works','Testimony','Contact']
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo/>
-            <Menu menuItems={items}/>
+            <Container>
+                <FlexWrapper  justify={'space-between'} align={'center'}>
+                    <Logo/>
+                    <HeaderMenu menuItems={items}/>
+                </FlexWrapper>
+
+            </Container>
+
         </StyledHeader>
     );
 };
 
 
 const StyledHeader = styled.header`
-    background-color: aquamarine;
-    display: flex;
-    justify-content: space-between;
+    //background: rgba(31, 31, 32, 0.9);
+    background: rgba(31, 31, 32, 0.2);
+    padding: 20px 0;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 999;
+  
 `
---
+++ src/styles/Global.styled.ts
@@ -0,0 +1,46 @@
+import {createGlobalStyle} from "styled-components";
+import {Theme} from "./Theme";
+
+
+export const GlobalStyle = createGlobalStyle`
+*, 
+*::before,
+*::after{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+    body {
+        margin: 0;
+        font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: ${Theme.colors.font}
+    }
+    
+    a {
+        text-decoration: none;
+    }
+    
+    ul{
+        list-style: none;
+    }
+    
+    button{
+        background-color: unset;
+        border: none;
+    }
+    
+    section:nth-of-type(odd){
+        background-color: ${Theme.colors.primaryBg};
+    }
+    
+    section:nth-of-type(even) {
+        background-color: ${Theme.colors.secondaryBg};
+    }
+
+
+`

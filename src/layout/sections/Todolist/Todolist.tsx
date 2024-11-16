719a993412708d19798e482e0787828a1b7fd970
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-11-16 12:12:57 +0300

    Todolist/TodolistSkills2

--- src/layout/sections/Todolist/Todolist.tsx
+++ src/layout/sections/Todolist/Todolist.tsx
@@ -1,37 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from "styled-components";
 import {Container} from "../../../components/Container";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {TodolistSkills} from "./TodolistSkills";
 
+
+export type  FilterType = 'All' | 'Active' | 'Completed'
 export const Todolist = () => {
-    const name1 ='CSS '
-    const name2 = 'Фреймворки и библиотеки для создания UI'
-    const name3 ='инструменты сборки проектов'
-    const task1 = [
+    const name1 ='CSS'
+
+    const [tas, setTas] = useState([
         {id: 1, isDone: true, text: 'Flexbox' },
-        {id: 2, isDone: true, text:  'Grid' },
-        {id: 3, isDone: true, text: 'анимации'}
-    ]
-    const task2 = [
-        {id: 1, isDone: false, text: 'Vue' },
-        {id: 2, isDone: true, text:  'React' },
-        {id: 3, isDone: false, text: 'Angular'}
-    ]
-    const task3 = [
-        {id: 1, isDone: true, text: 'Webpack' },
-        {id: 2, isDone: false, text:  'Vite' },
-        {id: 3, isDone: false, text: 'Parcel'}
-    ]
+        {id: 2, isDone: false, text:  'Grid' },
+        {id: 3, isDone: true, text: 'анимации'},
+        {id: 4, isDone: true, text: 'Webpack' },
+        {id: 5, isDone: false, text:  'Vite' },
+        {id: 6, isDone: false, text: 'Parcel'},
+        {id: 7, isDone: false, text: 'Vue' },
+        {id: 8, isDone: true, text:  'React' },
+        {id: 9, isDone: false, text: 'Angular'}
+    ])
+
+    function removeTask(id: number) {
+        let resultTask = tas.filter((el) => {
+            return el.id !== id;
+        })
+        setTas(resultTask)
+    }
+
+
+    let [filt, setFilt] = useState<FilterType>('All')
+
+    function chanFilt(value: FilterType) {
+        setFilt(value)
+    }
+
+    let tasksForTodoList = tas;
+    if(filt === 'Completed') {
+        tasksForTodoList = tas.filter(el => el.isDone === true)
+    }
+    if(filt === 'Active') {
+        tasksForTodoList = tas.filter(el => el.isDone === false)
+    }
+
     return (
         <TodolistSection>
             <Container>
                 <FlexWrapper justify={'space-between'} align={'center'}>
-                    <TodolistSkills title={name1} tasks={task1}/>
-                    <TodolistSkills title={name2} tasks={task2}/>
-                    <TodolistSkills title={name3} tasks={task3}/>
+                    <TodolistSkills title={name1} tasks={tasksForTodoList} removeTask={removeTask} chanFilt={chanFilt}/>
                 </FlexWrapper>
-
             </Container>
 
         </TodolistSection>
--- src/layout/sections/Todolist/TodolistSkills.tsx
+++ src/layout/sections/Todolist/TodolistSkills.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
+import {FilterType} from "./Todolist";
 
 type TodolistSkillsProps = {
     id: number,
@@ -10,14 +11,17 @@ type TodolistSkillsProps = {
 type TodoListProps = {
     title?: string;
     tasks: TodolistSkillsProps[]
+    removeTask: (id: number) => void;
+    chanFilt: (value: FilterType) => void;
 }
 
-export const TodolistSkills = ({title, tasks}: TodoListProps) => {
+export const TodolistSkills = ({title, tasks, removeTask, chanFilt}: TodoListProps) => {
 
     const task = tasks.map((el) => {
         return (
             <li key={el.id}>
                 <input type="checkbox" checked={el.isDone}/> <span>{el.text}</span>
+                <ButtonClik onClick={() => {removeTask(el.id)}}>+</ButtonClik>
             </li>
         )
     })
@@ -27,19 +31,17 @@ export const TodolistSkills = ({title, tasks}: TodoListProps) => {
             <h3>{title}</h3>
                 <div>
                     <input/>
-                    <button>+</button>
+                    <ButtonClik>+</ButtonClik>
                 </div>
                 <ul>
                     {task}
-
                 </ul>
                 <ButtonDiv>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
+                    <button onClick={() => {chanFilt('All')}}>All</button>
+                    <button onClick={() => {chanFilt('Active')}}>Active</button>
+                    <button onClick={() => {chanFilt('Completed')}}>Completed</button>
                 </ButtonDiv>
             </TodoSkillslist>
-
     );
 };
 const TodoSkillslist = styled.div`
@@ -58,14 +60,19 @@ const TodoSkillslist = styled.div`
         padding: 0 20px 0 20px;
     }
 `
+const ButtonClik = styled.button`
+    background-color:  #7365cb;
+    margin: 8px auto 8px;
 
+    padding: 6px 10px 6px 10px;
+    border-radius: 6px;
+`
 const ButtonDiv = styled.div`
     background-color: #5a0b0b;
 
     button {
         background-color:  #7365cb;
         margin: 8px auto 8px;
-        list-style: none;
         padding: 8px 15px 8px 15px;
         border-radius: 6px;
     }
